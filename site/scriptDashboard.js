e330548c9a4bfa10fbaf1eaa57ac43f6ad0ffb97
fabiamdamaceno <fabiam.silva@sptech.school>
2024-11-23 18:42:00 -0300

    Rascunho da Dashboard

--- site/scriptDashboard.js
+++ site/scriptDashboard.js
@@ -1,7 +1,151 @@
-function gerarLeituras() {
-    alert(1)
-};
+const ctx1 = document.getElementById('myChart1');
+const ctx2 = document.getElementById('myChart2');
+const ctx3 = document.getElementById('myChart3');
+const ctx4 = document.getElementById('myChart4');
 
-gerarLeituras();
+var qtdLivros = ['0', '1', '2 - 4', '5 - 9', '10 ou mais'];
+var qtdPessoas = [12, 19, 3, 5, 2, 3];
 
-// abrirAdicionarLeitura() - função a ser definida
+new Chart(ctx1, {
+    type: 'bar',
+    data: {
+        labels: qtdLivros,
+        datasets: [{
+            label: 'Quantidade de pessoas',
+            data: qtdPessoas,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Leitura em 2023'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de Pessoas'
+                }
+            },
+            x: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de livros'
+                }
+            }
+        }
+    }
+});
+
+new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: qtdLivros,
+        datasets: [{
+            label: 'Quantidade de pessoas',
+            data: qtdPessoas,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Leitura em 2023'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de Pessoas'
+                }
+            },
+            x: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de livros'
+                }
+            }
+        }
+    }
+});
+
+new Chart(ctx3, {
+    type: 'bar',
+    data: {
+        labels: qtdLivros,
+        datasets: [{
+            label: 'Quantidade de pessoas',
+            data: qtdPessoas,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Leitura em 2023'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de Pessoas'
+                }
+            },
+            x: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de livros'
+                }
+            }
+        }
+    }
+});
+
+new Chart(ctx4, {
+    type: 'bar',
+    data: {
+        labels: qtdLivros,
+        datasets: [{
+            label: 'Quantidade de pessoas',
+            data: qtdPessoas,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Leitura em 2023'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de Pessoas'
+                }
+            },
+            x: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Quantidade de livros'
+                }
+            }
+        }
+    }
+});
