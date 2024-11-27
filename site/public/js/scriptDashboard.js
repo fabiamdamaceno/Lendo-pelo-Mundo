ac74242e9e6c397a7dc99b319a95f928447a5810
fabiamdamaceno <fabiam.silva@sptech.school>
2024-11-27 14:16:09 -0300

    Alterado graficos Dashboard

--- site/public/js/scriptDashboard.js
+++ site/public/js/scriptDashboard.js
@@ -6,13 +6,16 @@ const ctx4 = document.getElementById('myChart4');
 var qtdLivros = ['0', '1', '2 - 4', '5 - 9', '10 ou mais'];
 var qtdPessoas = [12, 19, 3, 5, 2, 3];
 
+var c1_mes = ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+var c1_livrosMes = [1, 3, 4, 1, 2, 1, 2, 1, 2, 1, 2, 4]
+
 new Chart(ctx1, {
     type: 'bar',
     data: {
-        labels: qtdLivros,
+        labels: c1_mes,
         datasets: [{
-            label: 'Quantidade de pessoas',
-            data: qtdPessoas,
+            label: 'Livros',
+            data: c1_livrosMes,
             borderWidth: 1
         }]
     },
@@ -20,7 +23,7 @@ new Chart(ctx1, {
         plugins: {
             title: {
                 display: true,
-                text: 'Leitura em 2023'
+                text: 'Leitura Anual'
             }
         },
         scales: {
@@ -28,14 +31,14 @@ new Chart(ctx1, {
                 beginAtZero: true,
                 title: {
                     display: true,
-                    text: 'Quantidade de Pessoas'
+                    text: 'Livros Lidos'
                 }
             },
             x: {
                 beginAtZero: true,
                 title: {
                     display: true,
-                    text: 'Quantidade de livros'
+                    text: 'Meses'
                 }
             }
         }
